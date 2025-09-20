506f7aac87cd708c59d8eb665b9eac8d1c472d49
FranciscoG1803 <00077225@uca.edu.sv>
2025-09-19 22:38:44 -0600

    :tada: feature: Ejercicio 5

---
+++ Doc.cpp
@@ -0,0 +1,26 @@
+/*5. Structs básicos
+Crea un struct Estudiante que almacene: nombre, edad y promedio.
+    - Pide al usuario los datos de 3 estudiantes.
+    - Imprime cuál tiene el mejor promedio.*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int total_estudiantes = 3;
+
+struct Estudiante
+{
+    string nombre;
+    int edad;
+    float promedio;
+};
+
+
+int main(){
+
+
+    
+    return 0;
+}
